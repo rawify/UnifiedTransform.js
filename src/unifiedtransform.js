9ffe08acc3bdaa74ef6e9f3e2c21c9015a5a67d0
Robert Eisele <robert@xarg.org>
2024-10-06 12:05:56 +0200

    Fixed min file

--- src/unifiedtransform.js
+++ src/unifiedtransform.js
@@ -1,5 +1,5 @@
 /**
- * @license UnifiedTransform v0.1.1 10/5/2024
+ * @license UnifiedTransform v0.2.0 10/6/2024
  * https://github.com/rawify/UnifiedTransform.js
  *
  * Copyright (c) 2024, Robert Eisele (https://raw.org/)
@@ -8,12 +8,11 @@
 
 const TAU = Math.PI * 2;
 
-class UnifiedTransform {
+function UnifiedTransform() {
+    this['matrix'] = [1, 0, 0, 1, 0, 0];
+}
 
-    constructor() {
-        this.matrix = [1, 0, 0, 1, 0, 0];
-    }
-
+UnifiedTransform.prototype = {
     /**
      * Specifies a 2D translation by the vector (tx, ty)
      * 
@@ -21,11 +20,11 @@ class UnifiedTransform {
      * @param {number|string} ty The number to be translated on the x-axis.
      * @returns 
      */
-    translate(tx, ty) {
-        return this.applyMatrix([
+    translate: function (tx, ty) {
+        return this['applyMatrix']([
             1, 0, 0,
             1, parseFloat(String(tx).replace("px", "")) || 0, parseFloat(String(ty).replace("px", "")) || 0]);
-    }
+    },
 
     /**
      * Specifies a 2D scale operation by the (sx, sy) scaling vector described by the 2 parameters
@@ -34,16 +33,16 @@ class UnifiedTransform {
      * @param {number|string|null} sy The number to be scaled on the y-axis.
      * @returns 
      */
-    scale(sx, sy = null) {
+    scale: function (sx, sy = null) {
 
         if (sy === null) {
             sy = sx;
         }
 
-        return this.applyMatrix([
+        return this['applyMatrix']([
             parseFloat(sx) || 1, 0, 0,
             parseFloat(sy) || 1, 0, 0]);
-    }
+    },
 
     /**
      * Specifies a 2D rotation by the angle specified in the parameter about the origin of the element or optionally the given point (x, y)
@@ -53,7 +52,7 @@ class UnifiedTransform {
      * @param {number|string} y The y-coordinate of the point to be rotated around
      * @returns 
      */
-    rotate(angle, x, y) {
+    rotate: function (angle, x, y) {
 
         if (typeof angle == "string") {
 
@@ -73,16 +72,16 @@ class UnifiedTransform {
         let sin = Math.sin(angle);
 
         if (x !== undefined) {
-            this.translate(x, y || 0);
+            this['translate'](x, y || 0);
         }
 
-        this.applyMatrix([cos, sin, -sin, cos, 0, 0]);
+        this['applyMatrix']([cos, sin, -sin, cos, 0, 0]);
 
         if (x !== undefined) {
-            return this.translate(-x, -(y || 0));
+            return this['translate'](-x, -(y || 0));
         }
         return this;
-    }
+    },
 
     /**
      * Specifies a 2D skew transformation along the X axis by the given angle.
@@ -90,11 +89,11 @@ class UnifiedTransform {
      * @param {number|string} degrees The skew angle
      * @returns UnifiedTransform
      */
-    skewX(degrees) {
+    skewX: function (degrees) {
         let tan = Math.tan(parseFloat(degrees) / 360 * TAU);
 
-        return this.applyMatrix([1, 0, tan, 1, 0, 0]);
-    }
+        return this['applyMatrix']([1, 0, tan, 1, 0, 0]);
+    },
 
     /**
      * Specifies a 2D skew transformation along the Y axis by the given angle.
@@ -102,12 +101,12 @@ class UnifiedTransform {
      * @param {number|string} degrees The skew angle
      * @returns UnifiedTransform
      */
-    skewY(degrees) {
+    skewY: function (degrees) {
 
         let tan = Math.tan(parseFloat(degrees) / 360 * TAU);
 
-        return this.applyMatrix([1, tan, 0, 1, 0, 0]);
-    }
+        return this['applyMatrix']([1, tan, 0, 1, 0, 0]);
+    },
 
     /**
      * Specifies a 2D transformation in the form of a transformation matrix of the six values [a, b, c, d, x, y]
@@ -115,12 +114,12 @@ class UnifiedTransform {
      * @param {Array} m The matrix given as a one dimensional array
      * @returns UnifiedTransform
      */
-    applyMatrix(m) {
+    applyMatrix: function (m) {
 
-        let [Aa, Ab, Ac, Ad, Ax, Ay] = this.matrix;
+        let [Aa, Ab, Ac, Ad, Ax, Ay] = this['matrix'];
         let [Ba, Bb, Bc, Bd, Bx, By] = m.map(parseFloat)
 
-        this.matrix = [ // = A * B
+        this['matrix'] = [ // = A * B
             Aa * Ba + Ac * Bb,
             Ab * Ba + Ad * Bb,
             Aa * Bc + Ac * Bd,
@@ -129,7 +128,7 @@ class UnifiedTransform {
             Ab * Bx + Ad * By + Ay]
 
         return this;
-    }
+    },
 
     /**
      * Parses a transform string and applies each transformation one after the other
@@ -137,7 +136,7 @@ class UnifiedTransform {
      * @param {string} str The CSS transform string
      * @returns UnifiedTransform
      */
-    transform(str) {
+    transform: function (str) {
 
         for (let m of [...str.matchAll(/(matrix|translate|translateX|translateY|scale|rotate|skewX|skewY)\s*\(([^)]+)\)/g)]) {
 
@@ -148,57 +147,57 @@ class UnifiedTransform {
             switch (cmd) {
                 case 'matrix':
                     if (params.length === 6) {
-                        this.applyMatrix(params);
+                        this['applyMatrix'](params);
                     }
                     break;
 
                 case 'translate':
                     if (params.length === 1) {
-                        this.translate(params[0], 0);
+                        this['translate'](params[0], 0);
                     } else if (params.length === 2) {
-                        this.translate(params[0], params[1]);
+                        this['translate'](params[0], params[1]);
                     }
                     break;
 
                 case 'translateX':
-                    this.translate(params[0], 0);
+                    this['translate'](params[0], 0);
                     break;
 
                 case 'translateY':
-                    this.translate(0, params[0]);
+                    this['translate'](0, params[0]);
                     break;
 
                 case 'scale':
                     if (params.length === 1) {
-                        this.scale(params[0], params[0]);
+                        this['scale'](params[0], params[0]);
                     } else if (params.length === 2) {
-                        this.scale(params[0], params[1]);
+                        this['scale'](params[0], params[1]);
                     }
                     break;
 
                 case 'rotate':
                     if (params.length === 1) {
-                        this.rotate(params[0], 0, 0);
+                        this['rotate'](params[0], 0, 0);
                     } else if (params.length === 3) {
-                        this.rotate(params[0], params[1], params[2]);
+                        this['rotate'](params[0], params[1], params[2]);
                     }
                     break;
 
                 case 'skewX':
                     if (params.length === 1) {
-                        this.skewX(params[0]);
+                        this['skewX'](params[0]);
                     }
                     break;
 
                 case 'skewY':
                     if (params.length === 1) {
-                        this.skewY(params[0]);
+                        this['skewY'](params[0]);
                     }
                     break;
             }
         }
         return this;
-    }
+    },
 
     /**
      * Evaluates a point using the current transformation matrix
@@ -207,13 +206,13 @@ class UnifiedTransform {
      * @param {number} y 
      * @returns The transformed point
      */
-    eval(x, y) {
-        const m = this.matrix;
+    eval: function (x, y) {
+        const m = this['matrix'];
         return [
             x * m[0] + y * m[2] + m[4],
             x * m[1] + y * m[3] + m[5]
         ];
-    }
+    },
 
     /**
      * Gets the 3x3 transform matrix of the current homogeneous transformation
@@ -221,8 +220,8 @@ class UnifiedTransform {
      * @param {boolean} d2 Decides if the returned matrix is a two dimensional array
      * @returns The current transform matrix
      */
-    toMatrix(d2 = false) {
-        const [a, b, c, d, tx, ty] = this.matrix;
+    toMatrix: function (d2 = false) {
+        const [a, b, c, d, tx, ty] = this['matrix'];
 
         if (d2) {
             return [
@@ -234,14 +233,14 @@ class UnifiedTransform {
             a, c, tx,
             b, d, ty,
             0, 0, 1];
-    }
+    },
 
     /**
      * Gets a CSS transform string to apply the current matrix
      * 
      * @returns The transform string
      */
-    toTransformString() {
-        return 'matrix(' + this.matrix.join(", ") + ')';
+    toTransformString: function () {
+        return 'matrix(' + this['matrix'].join(", ") + ')';
     }
 }
